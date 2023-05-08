5522b53c60a7127ef66830c1d34ca7663be5cbcf
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-05-08 11:30:55 +0300

    add new classes

---
+++ java/examples/AmphibianTest.java
@@ -0,0 +1,9 @@
+package examples;
+
+public class AmphibianTest {
+    public static void main(String[] args) {
+        Amphibian a = new Amphibian();
+        a.fly();
+        a.swim();
+    }
+}
---
+++ java/examples/Flyable.java
@@ -0,0 +1,5 @@
+package examples;
+
+public interface Flyable {
+    void fly();
+}
